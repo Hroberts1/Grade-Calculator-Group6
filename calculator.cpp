c40eb2ad28a6cc582bbb3f3dcae205b983664173
iamdadaism <159055715+iamdadaism@users.noreply.github.com>
2024-03-04 21:54:15 -0600

    Update calculator.cpp, with course_inf structure

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,7 @@
 #include "calculator.h"
 using namespace std;
 
-double calcAvg(CourseInfo c){
+double calcAvg(course_inf c){
     double quizavg = 0;
     double examavg = 0;
     double homeworkavg = 0;
@@ -12,29 +12,37 @@ double calcAvg(CourseInfo c){
     int assignmentnum = 0;
 
     for(auto it = c.assignments.begin(); it != c.assignments.end(); it++){
-        if(it -> assignmentType == "quiz"){
-            quizavg += it->grade;
+        if((*it) -> type == "quiz"){
+            quizavg += (*it)->grade;
+            quiznum += 1;
         }
-        else if(it -> assignmentType == "exam"){
-            examavg += it->grade;
+        else if((*it) -> type == "exam"){
+            examavg += (*it)->grade;
+            examnum += 1;
         }
-        else if(it -> assignmentType == "homework"){
-            homeworkavg += it->grade;
+        else if((*it) -> type == "homework"){
+            homeworkavg += (*it)->grade;
+            homeworknum += 1;
         }
-        else if(it -> assignmentType == "assignment"){
-            assignmentavg += it->grade;
+        else if((*it) -> type == "assignment"){
+            assignmentavg += (*it)->grade;
+            assignmentnum += 1;
         }
     }
     quizavg /= quiznum;
+    // cout << "quizavg: " << quizavg << endl;
     examavg /= examnum;
+    // cout << "examnum: " << examavg << endl;
     homeworkavg /= homeworknum;
+    // cout << "homeworkavg: " << homeworkavg << endl;
     assignmentavg /= assignmentnum;
+    // cout << "assignmentavg: " << assignmentavg << endl;
 
     double totavg = quizavg * c.quizWeight + examavg * c.examWeight + homeworkavg * c.homeworkWeight + assignmentavg * c.assignmentWeight;
     return totavg;
 }
 
-double calcNeededQuizAvg(double grade, CourseInfo c, int totquiznum){
+double calcNeededQuizAvg(double grade, course_inf c){
     double examavg = 0;
     double homeworkavg = 0;
     double assignmentavg = 0;
@@ -43,27 +51,33 @@ double calcNeededQuizAvg(double grade, CourseInfo c, int totquiznum){
     int assignmentnum = 0;
 
     for(auto it = c.assignments.begin(); it != c.assignments.end(); it++){
-        if(it -> assignmentType == "exam"){
-            examavg += it->grade;
+        if((*it) -> type == "exam"){
+            examavg += (*it)->grade;
+            examnum += 1;
         }
-        else if(it -> assignmentType == "homework"){
-            homeworkavg += it->grade;
+        else if((*it) -> type == "homework"){
+            homeworkavg += (*it)->grade;
+            homeworknum += 1;
         }
-        else if(it -> assignmentType == "assignment"){
-            assignmentavg += it->grade;
+        else if((*it) -> type == "assignment"){
+            assignmentavg += (*it)->grade;
+            assignmentnum += 1;
         }
     }
     examavg /= examnum;
+    // cout << "examnum: " << examavg << endl;
     homeworkavg /= homeworknum;
+    // cout << "homeworkavg: " << homeworkavg << endl;
     assignmentavg /= assignmentnum;
+    // cout << "assignmentavg: " << assignmentavg << endl;
 
     double neededavg = grade - (examavg * c.examWeight + homeworkavg * c.homeworkWeight + assignmentavg * c.assignmentWeight);
-    neededavg /= totquiznum;
-
+    // cout << "Needed Total: " << neededavg << endl;
+    neededavg /= c.quizWeight;
     return neededavg;
 }
 
-double calcNeededExamAvg(double grade, CourseInfo c, int totexamnum){
+double calcNeededExamAvg(double grade, course_inf c){
     double quizavg = 0;
     double homeworkavg = 0;
     double assignmentavg = 0;
@@ -72,14 +86,17 @@ double calcNeededExamAvg(double grade, CourseInfo c, int totexamnum){
     int assignmentnum = 0;
 
     for(auto it = c.assignments.begin(); it != c.assignments.end(); it++){
-        if(it -> assignmentType == "quiz"){
-            quizavg += it->grade;
+        if((*it) -> type == "quiz"){
+            quizavg += (*it)->grade;
+            quiznum += 1;
         }
-        else if(it -> assignmentType == "homework"){
-            homeworkavg += it->grade;
+        else if((*it) -> type == "homework"){
+            homeworkavg += (*it)->grade;
+            homeworknum += 1;
         }
-        else if(it -> assignmentType == "assignment"){
-            assignmentavg += it->grade;
+        else if((*it) -> type == "assignment"){
+            assignmentavg += (*it)->grade;
+            assignmentnum += 1;
         }
     }
     quizavg /= quiznum;
@@ -87,12 +104,12 @@ double calcNeededExamAvg(double grade, CourseInfo c, int totexamnum){
     assignmentavg /= assignmentnum;
 
     double neededavg = grade - (quizavg * c.quizWeight + homeworkavg * c.homeworkWeight + assignmentavg * c.assignmentWeight);
-    neededavg /= totexamnum;
+    neededavg /= c.examWeight;
 
     return neededavg;
 }
 
-double calcNeededHomeworkAvg(double grade, CourseInfo c, int tothomeworknum){
+double calcNeededHomeworkAvg(double grade, course_inf c){
     double quizavg = 0;
     double examavg = 0;
     double assignmentavg = 0;
@@ -101,14 +118,17 @@ double calcNeededHomeworkAvg(double grade, CourseInfo c, int tothomeworknum){
     int assignmentnum = 0;
 
     for(auto it = c.assignments.begin(); it != c.assignments.end(); it++){
-        if(it -> assignmentType == "quiz"){
-            quizavg += it->grade;
+        if((*it) -> type == "quiz"){
+            quizavg += (*it)->grade;
+            quiznum += 1;
         }
-        else if(it -> assignmentType == "exam"){
-            examavg += it->grade;
+        else if((*it) -> type == "exam"){
+            examavg += (*it)->grade;
+            examnum += 1;
         }
-        else if(it -> assignmentType == "assignment"){
-            assignmentavg += it->grade;
+        else if((*it) -> type == "assignment"){
+            assignmentavg += (*it)->grade;
+            assignmentnum += 1;
         }
     }
     quizavg /= quiznum;
@@ -116,12 +136,12 @@ double calcNeededHomeworkAvg(double grade, CourseInfo c, int tothomeworknum){
     assignmentavg /= assignmentnum;
 
     double neededavg = grade - (quizavg * c.quizWeight + examavg * c.examWeight + assignmentavg * c.assignmentWeight);
-    neededavg /= tothomeworknum;
+    neededavg /= c.homeworkWeight;
 
     return neededavg;
 }
 
-double calcNeededAssignmentAvg(double grade, CourseInfo c, int totassignmentnum){
+double calcNeededAssignmentAvg(double grade, course_inf c){
     double quizavg = 0;
     double examavg = 0;
     double homeworkavg = 0;
@@ -130,14 +150,17 @@ double calcNeededAssignmentAvg(double grade, CourseInfo c, int totassignmentnum)
     int homeworknum = 0;
 
     for(auto it = c.assignments.begin(); it != c.assignments.end(); it++){
-        if(it -> assignmentType == "quiz"){
-            quizavg += it->grade;
+        if((*it) -> type == "quiz"){
+            quizavg += (*it)->grade;
+            quiznum += 1;
         }
-        else if(it -> assignmentType == "exam"){
-            examavg += it->grade;
+        else if((*it) -> type == "exam"){
+            examavg += (*it)->grade;
+            examnum += 1;
         }
-        else if(it -> assignmentType == "homework"){
-            homeworkavg += it->grade;
+        else if((*it) -> type == "homework"){
+            homeworkavg += (*it)->grade;
+            homeworknum += 1;
         }
     }
     quizavg /= quiznum;
@@ -145,7 +168,7 @@ double calcNeededAssignmentAvg(double grade, CourseInfo c, int totassignmentnum)
     homeworkavg /= homeworknum;
 
     double neededavg = grade - (quizavg * c.quizWeight + examavg * c.examWeight + homeworkavg * c.homeworkWeight);
-    neededavg /= totassignmentnum;
+    neededavg /= c.assignmentWeight;
 
     return neededavg;
 }
